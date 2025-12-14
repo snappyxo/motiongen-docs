e4e6719356043f045fe459a5750f39808de1617e
Wei Liao <weiliao715@gmail.com>
2025-12-13 23:55:39 -0500

    update: configure for GitHub Pages deployment with custom domain

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,11 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://motiongen.io',
+  url: 'https://docs.motiongen.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
+  trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
